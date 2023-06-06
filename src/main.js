89f28079bd30b18cbfc0afc0c99fea0ebce0a8c2
michaelmehr <michaelmehr0@gmail.com>
2023-06-06 18:13:03 -0500

    changed 'append' to 'appendChild' in main.js styled grid items

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ function generateGrid(gridSize = 16) {
     for (let row = 1; row <= gridSize; row++) {
         const gridRow = document.createElement('div');
         gridRow.classList.add("grid-row", "flex");
-        container.append(gridRow);
+        container.appendChild(gridRow);
         for (let col = 1; col <= gridSize; col++) {
             const gridItem = document.createElement('div');
             gridItem.classList.add("grid-item");
-            gridItem.textContent = "a";
-            gridRow.append(gridItem);
+            gridRow.appendChild(gridItem);
         }
     }
 }
